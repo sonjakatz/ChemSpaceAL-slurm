f71386fc0115bb0b0c59a085faadbe40737657c8
sonja EMC <s.katz@erasmusmc.nl>
2024-07-11 12:15:53 +0200

    fixed auto creation of /results/

--- scripts/00_initaliseDir.py
+++ scripts/00_initaliseDir.py
@@ -9,6 +9,7 @@ OUTNAME = sys.argv[1]
 
 os.chdir('..')
 base_path = f"{os.getcwd()}/results/{OUTNAME}/"
+os.makedirs(base_path, exist_ok=True)
 
 InitializeWorkspace.create_folders(base_path=base_path)
 
